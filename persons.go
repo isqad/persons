7191c96c1fbb1a99b34288a364b2618ade33830e
Andrew N. Shalaev <isqad88@gmail.com>
2018-11-30 13:39:16 +0500

    added passport

--- persons.go
+++ persons.go
@@ -2,6 +2,7 @@ package persons
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"math/rand"
 	"os"
@@ -13,14 +14,27 @@ func init() {
 }
 
 type FakePerson struct {
-	name     string
-	surname  string
-	sname    string
-	birthday time.Time
+	Name              string
+	Surname           string
+	Sname             string
+	Birthday          time.Time
+	PassportSerial    string
+	PassportNumber    string
+	PassportIssueDate time.Time
+	PassportIssuer    string
 }
 
 func GetPerson() FakePerson {
-	return FakePerson{randLine(`./names_ru`), randLine(`./families_ru`), randLine(`./snames_ru`), randate()}
+	issuers := passportIssuers()
+	return FakePerson{
+		randLine(`./names_ru`),
+		randLine(`./families_ru`),
+		randLine(`./snames_ru`),
+		randate(1975, 1989),
+		randPassportSerial(),
+		randPassportNumber(),
+		randate(1994, 2010),
+		issuers[rand.Intn(len(issuers)-1)]}
 }
 
 func randLine(path string) string {
@@ -32,9 +46,31 @@ func randLine(path string) string {
 	return items[rand.Intn(len(items)-1)]
 }
 
-func randate() time.Time {
-	min := time.Date(1975, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
-	max := time.Date(1989, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
+func randPassportNumber() string {
+	number1 := [2]int{7, 8}
+	number2 := [2]int{5, 6}
+	number3 := [3]int{0, 1, 2}
+	number4 := [4]int{2, 3, 6, 8}
+
+	return fmt.Sprintf("51%d%d%d%d",
+		number1[rand.Intn(len(number1)-1)],
+		number2[rand.Intn(len(number2)-1)],
+		number3[rand.Intn(len(number3)-1)],
+		number4[rand.Intn(len(number4)-1)])
+}
+
+func randPassportSerial() string {
+	serial1 := [2]int{0, 1}
+	serial2 := [3]int{7, 8, 9}
+
+	return fmt.Sprintf("65%d%d",
+		serial1[rand.Intn(len(serial1)-1)],
+		serial2[rand.Intn(len(serial2)-1)])
+}
+
+func randate(startY int, endY int) time.Time {
+	min := time.Date(startY, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
+	max := time.Date(endY, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
 
 	sec := rand.Int63n(max-min) + min
 	return time.Unix(sec, 0)
@@ -56,4 +92,12 @@ func readLines(path string) ([]string, error) {
 	}
 
 	return lines, scanner.Err()
+}
+
+func passportIssuers() [4]string {
+	issuers := [4]string{"ОУФМС России по Ханты-мансийскому автономному округу - Югре в Сургутском районе",
+		"ОУФМС России по Нижегородской обл. в Ленинском р-не гор. Нижнего Новгорода",
+		"Октябрьским районным отделом внутренних дел города Самары",
+		"Отделом внутренних дел города Кулебаки Нижегородской области"}
+	return issuers
 }
